42fbc15ab62e5e855c822c3bb39761b5920561e7
Chris <chriscruz9000@proton.me>
2025-04-15 19:06:01 -0700

    Renamed file to Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class MyApplication {
+public class Application {
     @RequestMapping("/")
     String home() {
         return "Hello world!";
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
