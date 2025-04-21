2e04fce0709fee67d67f47e618505d2f2b4ffb8a
Chris <chriscruz9000@proton.me>
2025-04-21 11:51:22 -0700

    Implemented a POST endpoint

--- src/main/java/com/example/controllers/UsersController.java
+++ src/main/java/com/example/controllers/UsersController.java
@@ -10,4 +10,9 @@ public class UsersController {
     public User getGreeting(@PathVariable String name) {
         return new User("0", name);
     }
+
+    @PostMapping()
+    public String postGreeting(@RequestBody User request){
+        return "Created user: " + request.getName();
+    }
 }
